41b63d0a3af75511201d04b739b3232f7eb6ba49
CynthiaaaaaaJin <yuxinj5@uw.edu>
2026-09-23 15:48:03 -0400

    Connect app to Supabase and display movies

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
-export default function Home() {
-  return (
-      <main>
-        <h1>Hello World</h1>
-      </main>
-  );
+import { supabase } from "@/lib/supabase";
+
+export default async function Home() {
+    const { data: movies, error } = await supabase
+        .from("movies")
+        .select("*")
+        .order("id");
+
+    if (error) {
+        return (
+            <main>
+                <h1>Error loading movies</h1>
+                <p>{error.message}</p>
+            </main>
+        );
+    }
+
+    return (
+        <main
+            style={{
+                maxWidth: "700px",
+                margin: "50px auto",
+                fontFamily: "Arial",
+            }}
+        >
+            <h1>My Favorite Movies</h1>
+
+            <ul>
+                {movies?.map((movie) => (
+                    <li key={movie.id}>
+                        {movie.title} — {movie.year}
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
